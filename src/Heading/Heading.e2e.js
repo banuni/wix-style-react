844bdab9d2775fdf11cd56e182d66429d5cdbb34
Lior Belinsky <liorbe@wix.com>
2018-07-17 16:51:26 +0300

    revert e2e improvements

--- src/Heading/Heading.e2e.js
+++ src/Heading/Heading.e2e.js
@@ -3,16 +3,13 @@ import {getStoryUrl, waitForVisibilityOf} from 'wix-ui-test-utils/protractor';
 import {headingTestkitFactory} from '../../testkit/protractor';
 import autoExampleDriver from 'wix-storybook-utils/AutoExampleDriver';
 
-describe('Heading', () => {
+fdescribe('Heading', () => {
   let driver;
   const storyUrl = getStoryUrl('1. Foundation', '1.3 Heading');
   const dataHook = 'storybook-heading';
 
   beforeAll(async () => {
     await browser.get(storyUrl);
-  });
-
-  beforeEach(async () => {
     driver = headingTestkitFactory({dataHook});
     await waitForVisibilityOf(driver.element(), 'Cannot find Heading');
   });
@@ -21,12 +18,5 @@ describe('Heading', () => {
 
   eyes.it('should display correct content', () => {
     expect(driver.getText()).toBe('Hey there, good looking');
-  });
-
-  describe('light', () => {
-    eyes.it('should render light component', async () => {
-      await autoExampleDriver.setProps({light: true});
-      expect(driver.element().isPresent()).toBe(true);
-    });
   });
 });
