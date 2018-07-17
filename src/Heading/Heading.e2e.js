0f6c43722bc8d69ba442c507695a3be23686408f
Lior Belinsky <liorbe@wix.com>
2018-07-17 16:12:39 +0300

    add eyes test for light prop

--- src/Heading/Heading.e2e.js
+++ src/Heading/Heading.e2e.js
@@ -1,16 +1,32 @@
 import eyes from 'eyes.it';
 import {getStoryUrl, waitForVisibilityOf} from 'wix-ui-test-utils/protractor';
 import {headingTestkitFactory} from '../../testkit/protractor';
+import autoExampleDriver from 'wix-storybook-utils/AutoExampleDriver';
 
 describe('Heading', () => {
+  let driver;
   const storyUrl = getStoryUrl('1. Foundation', '1.3 Heading');
+  const dataHook = 'storybook-heading';
 
-  beforeEach(() => browser.get(storyUrl));
+  beforeAll(async () => {
+    await browser.get(storyUrl);
+  });
+
+  beforeEach(async () => {
+    driver = headingTestkitFactory({dataHook});
+    await waitForVisibilityOf(driver.element(), 'Cannot find Heading');
+  });
+
+  afterEach(() => autoExampleDriver.reset());
+
   eyes.it('should display correct content', () => {
-    const dataHook = 'storybook-heading';
-    const driver = headingTestkitFactory({dataHook});
+    expect(driver.getText()).toBe('Hey there, good looking');
+  });
 
-    return waitForVisibilityOf(driver.element(), 'Cannot find Heading')
-      .then(() => expect(driver.getText()).toBe('Hey there, good looking'));
+  describe('light', () => {
+    eyes.it('should render light component', async () => {
+      await autoExampleDriver.setProps({light: true});
+      expect(driver.element().isPresent()).toBe(true);
+    });
   });
 });
