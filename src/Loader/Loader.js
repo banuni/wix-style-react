ad07657654edf24262ef07beff0e015f3af20c4f
Shlomi Toussia-Cohen <shlomitc@wix.com>
2018-07-25 17:50:17 +0300

    migrate Loader

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 import WixComponent from '../BaseComponents/WixComponent';
 import Arc from './Arc';
 import css from './Loader.scss';
-import Text from '../Deprecated/Text';
+import Badge from '../Badge';
 import Tooltip from '../Tooltip';
 import FormFieldError from '../new-icons/system/FormFieldError';
 import FormFieldErrorSmall from '../new-icons/system/FormFieldErrorSmall';
@@ -130,7 +130,7 @@ export default class Loader extends WixComponent {
         {
           shouldShowText && text &&
           <div className={css.text}>
-            <Text appearance="T5" dataHook="loader-text">{this.props.text}</Text>
+            <Badge type="transparent" dataHook="loader-text">{this.props.text}</Badge>
           </div>
         }
       </div>
